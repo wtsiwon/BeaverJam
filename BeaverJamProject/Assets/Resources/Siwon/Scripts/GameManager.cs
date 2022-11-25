5547ed5528073b5da04baceaa5447a339344a010
wtsiwon <siwon4727@gmail.com>
2022-11-26 07:51:14 +0900

    GM

--- BeaverJamProject/Assets/Resources/Siwon/Scripts/GameManager.cs
+++ BeaverJamProject/Assets/Resources/Siwon/Scripts/GameManager.cs
@@ -8,12 +8,47 @@ public class GameManager : Singleton<GameManager>
 
 
     public Coroutine spCoroutine;
+    public bool isGameStart;
+
+    [Tooltip("??????? ???Spd: ???, ??????")]
+    public float movingElementSpd;
+
+    [Tooltip("?¥í??? Spd")]
+    public float boostingSpd;
+
+    public GameObject pauseBoard;
+
+    private bool isPauseBoard;
+    public bool IsPauseBoard
+    {
+        get => isPauseBoard;
+        set
+        {
+            isPauseBoard = value;
+
+            pauseBoard.SetActive(isPauseBoard);
+        }
+    }
+    private void Start()
+    {
+        isGameStart = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsPauseBoard = !IsPauseBoard;
+        }
+    }
+
+
     public void StartSetting()
     {
-        if(spCoroutine != null) StopCoroutine(spCoroutine);
+        if (spCoroutine != null) StopCoroutine(spCoroutine);
 
         Spawner.Instance.Clear();
-        spCoroutine = StartCoroutine(Spawner.Instance.Spawn());
+        spCoroutine = Spawner.Instance.StartCoroutine(nameof(Spawner.Instance.Spawn));
     }
 
     public void GameOver()
